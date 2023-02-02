0aa9d298a41ef72ab83c6112f8396762c9aecf50
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-01 21:14:11 -0700

    Added SQL file

---
+++ dump-cli_social_media-202302012110.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 35.222.8.104    Database: cli_social_media
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.11.1-MariaDB-1:10.11.1+maria~deb11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `exploits`
+--
+
+DROP TABLE IF EXISTS `exploits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exploits` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `content` varchar(100) DEFAULT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `exploits_FK` (`user_id`),
+  CONSTRAINT `exploits_FK` FOREIGN KEY (`user_id`) REFERENCES `hackers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exploits`
+--
+
+LOCK TABLES `exploits` WRITE;
+/*!40000 ALTER TABLE `exploits` DISABLE KEYS */;
+INSERT INTO `exploits` VALUES (1,'Facebook had no chance.',3),(2,'Revamped popular game',6),(3,'Created a dozen mods for top console game',5),(4,'Stock market crash?  That was me.',1),(5,'Fast way into the dark web',2),(6,'Almost got into NASA.  It was out of this world',1),(7,'Lotto commission had me scratching my head',4),(8,'Everyone gets free internet and cable',3),(9,'Who needs cell service.  Not me',6),(10,'5 tips to get into a server',4),(11,'Problem with a crimial record?  I can help',2);
+/*!40000 ALTER TABLE `exploits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hackers`
+--
+
+DROP TABLE IF EXISTS `hackers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hackers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `alias` varchar(40) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hackers`
+--
+
+LOCK TABLES `hackers` WRITE;
+/*!40000 ALTER TABLE `hackers` DISABLE KEYS */;
+INSERT INTO `hackers` VALUES (1,'Starman','Saturn'),(2,'Darkside','Midnight'),(3,'QueenBee','Yellow12'),(4,'MollyDoo','Eggsandwich'),(5,'Iceman','TreeTrunk'),(6,'Lunchbox','apple15');
+/*!40000 ALTER TABLE `hackers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cli_social_media'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-01 21:10:47
